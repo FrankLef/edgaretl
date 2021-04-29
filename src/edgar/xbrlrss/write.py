9f436785cc1df564413b66827be4e22e39627061
FrankLef <flefebvre01@hotmail.com>
2021-04-28 22:00:51 -0400

    test the write_rss module

--- src/edgar/xbrlrss/write.py
+++ src/edgar/xbrlrss/write.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 from datetime import datetime
+import requests
 
 
-def get_fn(path, period={'year': 0, 'month': 0}, prefix: str = 'edgar', ext: str = '.xml', check: bool = False) -> Path:
+def write_fn(dir: Path = Path.cwd(), period={'year': None, 'month': None}, prefix: str = 'xbrlrss',
+             ext: str = '.xml', check: bool = False) -> Path:
     """Write the full file name for the rss feed file
 
     Args:
-        path ([type]): Basic path exlcuding file name.
-        period (dict, optional): Year an dmonth of the rss feed. Defaults to {'year': 0, 'month': 0}.
+        dir (Path, optional): Basic path excluding file name. Created with write_dir().
+        period (dict, optional): Year and month of the rss feed. Defaults to {'year': None, 'month': None}.
+                                 When either is None, current year and month are used.
         prefix (str, optional): prefix of filename. Defaults to 'edgar'.
         ext (str, optional): Extension of the file. Defaults to '.xml'.
         check (bool, optional): TRUE=Error if file already exists, FALSE=do nothing. Defaults to False.
@@ -18,35 +21,65 @@ def get_fn(path, period={'year': 0, 'month': 0}, prefix: str = 'edgar', ext: str
     Returns:
         Path: Full name where the xbrl file will be saved.
     """
-    a_file = '-'.join((prefix, str(period['year']),
-                       str(period['month']).zfill(2))) + ext
-    a_file = path.joinpath(a_file)
-    if check and path.exists:
+    if not isinstance(dir, Path):
+        msg = ("The data directory path is missing. Must be a pathlib.Path object.")
+        raise ValueError(msg)
+    if period['year'] is None or period['month'] is None:
+        now = datetime.now()
+        period['year'] = now.year
+        period['month'] = now.month
+
+    a_file = prefix + '_' + '-'.join((str(period['year']),
+                                     str(period['month']).zfill(2))) + ext
+    a_file = dir.joinpath(a_file)
+    if check and a_file.exists:
         msg = "File already exists. Delete it or use another name."
         msg = f"\n{msg}\n\"{a_file}\"\n"
         raise ValueError(msg)
     return a_file
 
 
-def get_dir(dir: str = 'data/xbrlrss', year: int = None) -> Path:
+def write_dir(dir: Path = Path('data', 'xbrlrss'), year: int = None) -> Path:
+    """Write the full directory where xbrlsrss files will be stored
+
+    Args:
+        dir (Path, optional): Data directory. Defaults to Path('data', 'xbrlrss').
+        year (int, optional): Year representing subdirecory of data directory. Defaults to None.
+                              When None, current year is used.
+
+    Raises:
+        ValueError: The year is out-of-bound.
+
+    Returns:
+        Path: Full path of data directory with year subdirectory.
+    """
+    # must use current year if None
     year = datetime.now().year if year is None else year
-    a_path = Path(__file__).parent.resolve().joinpath(dir)
-    a_path = a_path.joinpath(str(year))
+    limits = (2000, 2030)  # the allowed range of years
+    if not year >= limits[0] and year <= limits[1]:
+        msg = f"The year {year} is not between {limits[0]} and {limits[1]}"
+        raise ValueError(msg)
+    a_path = Path.cwd()
+    a_path = a_path.joinpath(dir, str(year))
     # a_path.mkdir(parents=True, exist_ok=True)
     return a_path
 
 
-def write_url(url: str, prefix: str = 'xbrlrss', period: list = {'year': None, 'month': None}) -> str:
+def write_url(url: str = 'https://www.sec.gov/Archives/edgar/monthly', prefix: str = 'xbrlrss',
+              period: list = {'year': None, 'month': None}, ext: str = '.xml') -> str:
     """Write full url to of xbrl rss feed
 
     Args:
-        url (str): Basic url of rss site
+        url (str, optional): Basic url of rss site. Defaults to 'https://www.sec.gov/Archives/edgar/monthly'.
         prefix (str, optional): Prefix of xbrl rss. Defaults to 'xbrlrss'.
-        period (list, optional): Period in yyy-mm format. Defaults to {'year': 0, 'month': 0}.
+        period (list, optional): Period in yyyy-mm format. Defaults to {'year': None, 'month': None}.
+                                 When None, current year and month willl be used.
+        ext (str, optional): File extension. Defaults to '.xml'.
 
     Returns:
         str: Full url for xbrl rss feed.
     """
+    # assign default values
     if (period['year'] is None) or (period['month'] is None):
         now = datetime.now()
         period['year'] = now.year
@@ -54,5 +87,12 @@ def write_url(url: str, prefix: str = 'xbrlrss', period: list = {'year': None, '
 
     doc = "-".join((prefix, str(period['year']),
                     str(period['month']).zfill(2)))
-    url = '/'.join((url, doc)) + '.xml'
+    url = '/'.join((url, doc)) + ext
+    # validate the url before using it
+    try:
+        r = requests.head(url)
+    except requests.exceptions.ConnectionError as e:
+        msg = f"The url is invalid, usually caused by an inexistant url.\n{url}"
+        print(msg)
+        raise requests.exceptions.ConnectionError
     return url
--- tests/xbrlrss/test_writerss.py
+++ tests/xbrlrss/test_writerss.py
@@ -1,6 +1,62 @@
+import pytest
+from pathlib import Path
+from datetime import datetime
+import requests
 from src.edgar.xbrlrss import write
 
+EDGAR_URL = 'https://www.sec.gov/Archives/edgar/monthly'
 
-def test_writerss():
-    a = 1
-    assert a == 1
+
+def test_write_url():
+    """Test write_url() with defaults values
+    """
+    url = EDGAR_URL
+    now = datetime.now()
+    period = {'year': now.year, 'month': now.month}
+    doc = "-".join(('xbrlrss', str(period['year']),
+                   str(period['month']).zfill(2)))
+    target = '/'.join((url, doc)) + '.xml'
+    assert write.write_url() == target
+
+
+def test_write_url_err():
+    """Test write_url() with invalid url
+    """
+    url = 'https://' + '/'.join(('url', 'invalid'))
+    with pytest.raises(requests.exceptions.ConnectionError):
+        write.write_url(url=url)
+
+
+def test_write_dir():
+    """Test write_dir with default values.
+    """
+    year = datetime.now().year
+    target = Path.cwd()
+    target = target.joinpath('data', 'xbrlrss', str(year))
+    assert write.write_dir(year=year) == target
+
+
+def test_write_dir_err():
+    """Test write_dir with wrong year.
+    """
+    with pytest.raises(ValueError):
+        write.write_dir(year=1999)
+
+
+def test_write_fn():
+    """Test write_fn with default values
+    """
+    now = datetime.now()
+    a_dir = write.write_dir(year=now.year)
+    a_file = 'xbrlrss' + '_'
+    a_file = a_file + '-'.join((str(now.year), str(now.month).zfill(2)))
+    a_file = a_file + '.xml'
+    target = a_dir.joinpath(a_file)
+    assert write.write_fn(dir=a_dir) == target
+
+
+def test_write_fn_err():
+    """Test write-fn error: the dir arg must be a pathlib.Path object
+    """
+    with pytest.raises(ValueError):
+        write.write_fn(dir='wrong, must be a pathlib.Path object')
